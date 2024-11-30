5c0d341086db4c3ed14e5cd5c18fe850c9bf6aea
YakitoSX <joaquixd2@gmail.com>
2024-11-30 20:13:42 -0300

    Se agrega un tema gtk, se modifica y mejora el script de internet, se modifica el picom.conf (esperemos que no se siga bugueando)

--- config/bspwm/src/get_network_interface.sh
+++ config/bspwm/src/get_network_interface.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-interface=$(nmcli -t -f DEVICE,TYPE,STATE device status | grep -E 'wifi|ethernet' | grep 'connected' | cut -d: -f1)
+set -euo pipefail
+
+config_file="$HOME/.config/bspwm/src/config/system.ini"
+
+interface_info=$(nmcli -t -f DEVICE,TYPE,STATE device status | grep -E 'wifi|ethernet' | grep 'connected')
+interface=$(echo "$interface_info" | cut -d: -f1)
+connection_type=$(echo "$interface_info" | cut -d: -f2)
 
 if [[ -z "$interface" ]]; then
-  echo "none"
+  interface_line="sys_network_interface = "
+  connection_type_line="sys_connection_type = "
 else
-  echo "$interface"
+  interface_line="sys_network_interface = $interface"
+  if [[ "$connection_type" == "wifi" ]]; then
+    connection_type_line="sys_connection_type = wireless"
+  elif [[ "$connection_type" == "ethernet" ]]; then
+    connection_type_line="sys_connection_type = wired"
+  fi
 fi
+
+sed -i "/^sys_network_interface = /c\\$interface_line" "$config_file" || echo "$interface_line" >> "$config_file"
+sed -i "/^sys_connection_type = /c\\$connection_type_line" "$config_file" || echo "$connection_type_line" >> "$config_file"
