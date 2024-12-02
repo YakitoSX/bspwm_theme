d5ebc81e07fac37812d9c23878c6dcc57de281e2
YakitoSX <joaquixd2@gmail.com>
2024-12-02 16:42:56 -0300

    Se mejora por última vez el script de check_network, todavia hay que mejorar varias cosas (picom, eww widgets, dunst, polybar)

--- config/bspwm/src/check_network.sh
+++ config/bspwm/src/check_network.sh
@@ -3,22 +3,26 @@ set -euo pipefail
 
 config_file="$HOME/.config/bspwm/src/config/system.ini"
 
-# Check if networking is enabled
-if ! nmcli networking; then
+interface_info=$(nmcli -t -f DEVICE,TYPE,STATE device status | grep -E 'wifi|ethernet' | grep 'connected')
+
+if [[ -z "$interface_info" ]]; then
+    # No active interface
     icon=" "
-    color="#FF0000"
+    color="#FF0000"  # Red for disconnected
 else
-    # Check for connected interfaces
-    interface_info=$(nmcli -t -f DEVICE,TYPE,STATE device status | grep -E 'wifi|ethernet' | grep 'connected')
-    
-    if [[ -z "$interface_info" ]]; then
+    # Interface is connected, check for IP address
+    interface=$(echo "$interface_info" | cut -d: -f1)
+    ip_address=$(nmcli device show "$interface" | grep 'IP4.ADDRESS' | awk '{print $2}')
+
+    if [[ -z "$ip_address" ]]; then
+        # Interface connected, but no IP address, assuming no internet
         icon=" "
-        color="#FF0000"
+        color="#FFFF00"  # Yellow for connected but no internet
     else
-        interface=$(echo "$interface_info" | cut -d: -f1)
+        # Interface is connected with a valid IP address (internet available)
         icon=" "
-        color="#00FF00"
-        
+        color="#00FF00"  # Green for connected with internet
+
         interface_line="sys_network_interface = $interface"
         if grep -q "^sys_network_interface = " "$config_file"; then
             sed -i "/^sys_network_interface = /c\\$interface_line" "$config_file"
