aa4a4854e2e4c9b60b7e61d2954395a707d10653
YakitoSX <joaquixd2@gmail.com>
2024-12-02 15:14:50 -0300

    Se arreglan modulos

--- config/bspwm/src/check_network.sh
+++ config/bspwm/src/check_network.sh
@@ -3,22 +3,28 @@ set -euo pipefail
 
 config_file="$HOME/.config/bspwm/src/config/system.ini"
 
-interface_info=$(nmcli -t -f DEVICE,TYPE,STATE device status | grep -E 'wifi|ethernet' | grep 'connected')
-interface=$(echo "$interface_info" | cut -d: -f1)
-connection_type=$(echo "$interface_info" | cut -d: -f2)
-
-if [[ -z "$interface" ]]; then
-    icon=""
+# Check if networking is enabled
+if ! nmcli networking; then
+    icon=" "
     color="#FF0000"
 else
-    icon=""
-    color="#00FF00"
-    interface_line="sys_network_interface = $interface"
-
-    if grep -q "^sys_network_interface = " "$config_file"; then
-        sed -i "/^sys_network_interface = /c\\$interface_line" "$config_file"
+    # Check for connected interfaces
+    interface_info=$(nmcli -t -f DEVICE,TYPE,STATE device status | grep -E 'wifi|ethernet' | grep 'connected')
+    
+    if [[ -z "$interface_info" ]]; then
+        icon=" "
+        color="#FF0000"
     else
-        echo "$interface_line" >> "$config_file"
+        interface=$(echo "$interface_info" | cut -d: -f1)
+        icon=" "
+        color="#00FF00"
+        
+        interface_line="sys_network_interface = $interface"
+        if grep -q "^sys_network_interface = " "$config_file"; then
+            sed -i "/^sys_network_interface = /c\\$interface_line" "$config_file"
+        else
+            echo "$interface_line" >> "$config_file"
+        fi
     fi
 fi
 
